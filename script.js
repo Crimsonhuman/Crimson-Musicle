bd2e064b67d54bd4110b6d27611b5aaa8c68a5e6
Rifki Averil <72114369+Crimsonhuman@users.noreply.github.com>
2024-10-07 15:41:03 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,138 @@
+function openNav() {
+    document.getElementById("artistfo").style.width = "250px";
+   
+  }
+  
+  function closeNav() {
+    document.getElementById("artistfo").style.width = "0";
+   
+  }
+  ////////////
+  function openNavOMG() {
+    document.getElementById("OmgSong").style.width = "250px";
+   
+  }
+  function closeNavOmg() {
+    document.getElementById("OmgSong").style.width = "0";
+   
+  }
+    ///////////
+
+  function openNavPegging() {
+    document.getElementById("peggingSong").style.width = "250px";
+   
+  }
+  
+  function closeNavPegging() {
+    document.getElementById("peggingSong").style.width = "0";
+   
+  }
+    ///////////
+
+  function openNavReality() {
+    document.getElementById("RealClub").style.width = "250px";
+   
+  }
+  
+  function closeNavReality() {
+    document.getElementById("RealClub").style.width = "0";
+   
+  }
+    ///////////
+
+  function openNavPayung() {
+    document.getElementById("payungSong").style.width = "250px";
+   
+  }
+  
+  function closeNavPayung() {
+    document.getElementById("payungSong").style.width = "0";
+   
+  }
+    ///////////
+function openNavOddLoop() {
+    document.getElementById("oddSong").style.width = "250px";
+   
+  }
+  
+  function closeNavOddLoop() {
+    document.getElementById("oddSong").style.width = "0";
+   
+  }
+    ///////////
+    function openNavOdoriko() {
+      document.getElementById("odorikoSong").style.width = "250px";
+     
+    }
+    
+    function closeNavOdoriko() {
+      document.getElementById("odorikoSong").style.width = "0";
+     
+    }
+      ///////////
+
+// ////////////////////////////////////////////////////////////////////////////////
+
+
+function openNavNTR() {
+  document.getElementById("Netorare").style.width = "250px";
+ 
+}
+function closeNavNTR() {
+  document.getElementById("Netorare").style.width = "0";
+ 
+}
+  ///////////
+
+function openNavYoasobi() {
+  document.getElementById("YoasobiSong").style.width = "250px";
+ 
+}
+
+function closeNavYoasobi() {
+  document.getElementById("YoasobiSong").style.width = "0";
+ 
+}
+  ///////////
+
+function openNavHindia() {
+  document.getElementById("INDIA").style.width = "250px";
+ 
+}
+
+function closeNavHindia() {
+  document.getElementById("INDIA").style.width = "0";
+ 
+}
+  ///////////
+
+function openNavFifFif() {
+  document.getElementById("FIFTY").style.width = "250px";
+ 
+}
+
+function closeNavFifFif() {
+  document.getElementById("FIFTY").style.width = "0";
+ 
+}
+  ///////////
+function openNavJojo() {
+  document.getElementById("JOJO").style.width = "250px";
+ 
+}
+
+function closeNavJojo() {
+  document.getElementById("JOJO").style.width = "0";
+ 
+}
+
+///
+function openNavErial() {
+  document.getElementById("eri").style.width = "250px";
+ 
+}
+
+function closeNavErial() {
+  document.getElementById("eri").style.width = "0";
+ 
+}
